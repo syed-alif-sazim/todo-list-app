16e8fbad64330c0eaeae649b1393b52cfe1f2441
Syed Alif <syed.alif@sazim.io>
2024-12-07 06:27:57 +0600

    refactor(todo-12): Added appropriate type for function argument

--- modules/todo/utils/localStorage.ts
+++ modules/todo/utils/localStorage.ts
@@ -1,9 +1,11 @@
+import { ITask } from "./localStorage.interfaces";
+
 export const loadTasksFromLocalStorage = () => {
     const storedTasks = localStorage.getItem('tasks');
     return storedTasks ? JSON.parse(storedTasks) : null;
   };
   
-export const saveTasksToLocalStorage = (tasks: any[]) => {
+export const saveTasksToLocalStorage = (tasks: ITask[]) => {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 };
   
